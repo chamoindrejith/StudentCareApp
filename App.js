97c35f5b2d7f12495857331529c532b704d863b2
Chamodi <chamodiindrejith14@gmail.com>
2025-01-03 23:20:00 +0530

    fix : header, components

--- App.js
+++ App.js
@@ -1,49 +1,55 @@
-import { StyleSheet, Text, View } from "react-native";
 import { PaperProvider } from "react-native-paper";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { ScrollView } from "react-native";
-import { KeyboardAvoidingView } from "react-native";
 import Profile from "./components/Profile";
 import Course from "./components/Course";
 import Subjects from "./components/Subjects";
 
-
 const Stack = createNativeStackNavigator();
-export default function App() {
-  return (
-    <PaperProvider>
-      <SafeAreaView>
-        <ScrollView>
-          <KeyboardAvoidingView
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-          >
-            <NavigationContainer>
-              <RootStack />
-            </NavigationContainer>
-          </KeyboardAvoidingView>
-        </ScrollView>
-      </SafeAreaView>
-    </PaperProvider>
-  );
-}
+const headerOptions = {
+  title: "UoV Student Care",
+  headerStyle: {
+    backgroundColor: "#79027d",
+  },
+  headerTintColor: "#fff",
+  headerTitleStyle: {
+    fontWeight: "bold",
+  },
+  headerTitleAlign: "center",
+};
 
 function RootStack() {
   return (
-    <Stack.Navigator initialRouteName="Profile">
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="Course" component={Course} />
-      <Stack.Screen name="Subjects" component={Subjects} />
+    <Stack.Navigator
+      initialRouteName="Profile"
+      screenOptions={{
+        headerStyle: { backgroundColor: "#9370db" },
+      }}
+    >
+      <Stack.Screen name="Profile" component={Profile} options={headerOptions} />
+      <Stack.Screen
+        name="Course"
+        component={Course}
+        options={headerOptions}
+      />
+      <Stack.Screen
+        name="Subjects"
+        component={Subjects}
+        options={headerOptions}
+      />
     </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <PaperProvider>
+          <RootStack />
+        </PaperProvider>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
--- components/Profile.js
+++ components/Profile.js
@@ -1,7 +1,14 @@
+import { StyleSheet, View, ScrollView } from "react-native";
+import { PaperProvider, Text, Divider, Button } from "react-native-paper";
+
 export default function Profile() {
-    return(
+  return (
+    <PaperProvider>
+      <ScrollView>
         <View>
-            <Text>Profile</Text>
+        <Text variant="headlineLarge">Profile</Text>
         </View>
-    )
+      </ScrollView>
+    </PaperProvider>
+  );
 }
